05e8ac225faabb71de328f8a2dbe50e50470e547
girlealvarez <gialvarezg@unal.edu.co>
2024-03-08 18:46:24 -0500

    intento 1

--- clean_data.py
+++ clean_data.py
@@ -6,7 +6,8 @@ import pandas as pd
 
 def load_data(input_file):
     """Lea el archivo usando pandas y devuelva un DataFrame"""
-
+    df = pd.read_csv(input_file)
+    return df
 
 def create_fingerprint(df):
     """Cree una nueva columna en el DataFrame que contenga el fingerprint de la columna 'text'"""
@@ -21,22 +22,50 @@ def create_fingerprint(df):
     # 8. Ordene la lista de tokens y remueve duplicados
     # 9. Convierta la lista de tokens a una cadena de texto separada por espacios
 
+    df = df.copy()
+    df["key"] = df["text"]
+    df["key"] = df["key"].str.strip()
+    df["key"] = df["key"].str.lower()
+    df["key"] = df["key"].str.replace("-", "")
+    df["key"] = df["key"].str.translate(
+        str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+    )
+    df["key"] = df["key"].str.split()
+    stemmer = nltk.PorterStemmer()
+    # a la columna key le aplica la funcion a cada elemento de la columna
+    # la funcion lamdba que el argumento es X
+    # para cada palabra aplique el stemmer con la fucnión stem donde reduce la palabra a la raiz
+    df["key"] = df["key"].apply(lambda x: [stemmer.stem(word) for word in x])
+    df["key"] = df["key"].apply(lambda x: sorted(set(x))) # elimnina duplicados con set
+    df["key"] = df["key"].str.join(" ") # genera espacios en cada clave
+
+    return df
+
 
 def generate_cleaned_column(df):
     """Crea la columna 'cleaned' en el DataFrame"""
-
-    df = df.copy()
-
     # 1. Ordene el dataframe por 'fingerprint' y 'text'
     # 2. Seleccione la primera fila de cada grupo de 'fingerprint'
     # 3.  Cree un diccionario con 'fingerprint' como clave y 'text' como valor
     # 4. Cree la columna 'cleaned' usando el diccionario
+
+    df = df.copy()
+    df = df.sort_values(by=["key", "text"], ascending=[True, True])
+    keys = df.drop_duplicates(subset="key", keep="first")
+    key_dict = dict(zip(keys["key"], keys["text"]))
+    df["cleaned"] = df["key"].map(key_dict)
+
+    return df
 
 
 def save_data(df, output_file):
     """Guarda el DataFrame en un archivo"""
     # Solo contiene una columna llamada 'texto' al igual
     # que en el archivo original pero con los datos limpios
+    df = df.copy()
+    df = df[["cleaned"]]
+    df = df.rename(columns={"cleaned": "text"})
+    df.to_csv(output_file, index=False)
 
 
 def main(input_file, output_file):
